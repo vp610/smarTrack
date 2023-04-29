086e62ba7d9bba11b799741dd1af3c568457639e
abhinT <abhintomar1@gmail.com>
2023-04-29 14:03:15 -0400

    Fixed flask server

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_url_path='', static_folder='frontend/build')
+app = Flask(__name__, static_url_path='', static_folder='smartrack/build')
 
 @app.route("/", defaults={'path':''})
 def serve(path):
     return send_from_directory(app.static_folder,'index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
